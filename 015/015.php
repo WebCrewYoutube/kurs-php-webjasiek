98191e970409504ee92007817af79b7015e0db00
webjasiek <53037344+webjasiek@users.noreply.github.com>
2020-06-20 09:02:08 +0200

    Update 015.php

--- 015/015.php
+++ 015/015.php
@@ -8,5 +8,6 @@ $ob->authors();
 // przy okazji :
 $ob=100; // poszedł destruktor, bo do obiektu klasy Webcrew nie odnosi się już żadna referencja, zatem automatyczny zbieracz śmieci (garbage collection) zrobił swoje. Przy okazji, to nie rozumiem skąd ta nazwa. Moim zdaniem to powinna być nazwa:
 // *Destroyer of all worlds, annihilator of all beings* // - prawda, że lepsza?
+
 hr();
 ?>
